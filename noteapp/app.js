b04ac6c1b71cbe8c7916f6be1b2c1480373d0e64
Alan Gong <agong2@asu.edu>
2018-06-27 12:12:04 +0800

    all the work on section 3 so far

---
+++ noteapp/app.js
@@ -0,0 +1,47 @@
+console.log('Start app')
+
+const fs = require('fs')
+const _ = require('lodash')
+const yargs = require('yargs')
+// these are basically third party or Node objects with cool methods like .toString
+
+const notes = require('./notes.js')
+// these are your own objects (from your class files) with hopefully cool methods
+
+var textParse = yargs.argv
+// yargs is a third party and it parses text. this is a yargs object
+var argWhat = textParse._[0]
+console.log('Enter a command: ')
+console.log('Yargs', textParse) // you can see how yargs parses into arrays with this
+
+if (argWhat === 'add') {
+  console.log('Adding a note: ')
+  var note = notes.addNote(textParse.title, textParse.body)
+  if (note) {
+    console.log('Note "' + textParse.title + '" added!')
+  } else {
+    console.log('Note title already exists! ')
+  }
+} else if (argWhat === 'list') {
+  console.log('Listing all notes: ')
+  notes.listAll()
+} else if (argWhat === 'read') {
+  console.log('Reading note: ')
+  var y = notes.readNote(textParse.title)
+  if (y) { // basically says "if y is returned, then"
+    console.log('Title: ' + textParse.title)
+    console.log('Body: ' + y.body)
+  } else { // if no return is defined, the function doesn't returns undefined -- not a string!
+    console.log('"' + textParse.title + '" not found')
+  }
+} else if (argWhat === 'remove') {
+  console.log('Removing note: ')
+  var x = notes.removeNote(textParse.title)
+  if (x === true) {
+    console.log(textParse.title + ' deleted')
+  } else {
+    console.log(textParse.title + ' not found')
+  }
+} else {
+  console.log('Command not recognized')
+}
---
+++ noteapp/notes.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('noteappdata.json')
+    return JSON.parse(notesString)
+    // pulls the JSON notes, converts to JS objects, and stores in array
+    // writing to the JSON file will override old notes, so you have to pull em out
+  } catch (e) {
+    return []
+  }
+}
+
+var saveNote = (noteList) => {
+  fs.writeFileSync('noteappdata.json', JSON.stringify(noteList))
+  // this converts JS objects in array to JSON and stores in the .JSON file
+}
+
+// // // // // // // // // // // // // // // // // //
+
+var addNote = (title, body) => {
+  console.log(title, body)
+  var noteList = fetchNotes() // calls the fetchNotes function above
+  var note = {
+    title,
+    body
+  // object that stores the arguments
+  }
+
+  var duplicateNotes = noteList.filter((note) => note.title === title)
+  // filter copies to duplicate array based on parameters (in this case, if note.title = title)
+
+  if (duplicateNotes.length === 0) { // if note.title != title, nothing gets copied over)
+    noteList.push(note)
+    // this puts the new note object on the array with the old notes
+    saveNote(noteList) // calls the saveNotes function above
+    return note
+  }
+}
+
+var listAll = () => {
+  console.log()
+}
+
+var readNote = (title) => {
+  var noteList = fetchNotes()
+  var duplicateNotes = noteList.filter((note) => note.title === title)
+  // if note.title = title, it gets copied over
+  if (duplicateNotes.length !== 0) {
+    return duplicateNotes[0] // returns whatever gets copied over, if anything
+  }
+}
+
+var removeNote = (title) => {
+  var noteList = fetchNotes()
+  var duplicateNotes = noteList.filter((note) => note.title !== title)
+  // in this case, copies to duplicate array if note.title != title (if note.title = title, it doesn't get copied over)
+  saveNote(duplicateNotes)
+
+  return noteList.length !== duplicateNotes.length
+}
+
+module.exports = {
+  addNote,
+  listAll,
+  readNote,
+  removeNote
+}
